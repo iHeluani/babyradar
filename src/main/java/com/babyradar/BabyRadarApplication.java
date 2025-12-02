a809fc864f58c3ddb3c758a77051f40fcc74e2b2
iheluani <heluani@hotmail.com>
2025-12-02 10:10:57 +0100

    First commit.

---
+++ src/main/java/com/babyradar/BabyRadarApplication.java
@@ -0,0 +1,13 @@
+package com.babyradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabyRadarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/babyradar/BabyRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babyradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyRadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
